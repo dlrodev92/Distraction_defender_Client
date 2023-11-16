483ec38cd22bb9f1e97c011d7a18b774d113b838
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-11-16 13:45:07 +0000

    clientsetup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+     <div>
+        <h1 className='hello'>Hellos React App perro sanchez a prision</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
